b4bdf5a4f36c15e9511d55205543d8214113e9b7
scottbedard <scottbedard90@gmail.com>
2016-01-26 23:27:05 -0700

    v-link behavior for embedded links

--- assets/js/pages/blog/show.js
+++ assets/js/pages/blog/show.js
@@ -35,8 +35,30 @@ module.exports = {
      * @return {Object}
      */
     data() {
-        return {
-            post: {},
-        };
+        let post = {};
+        return { post };
+    },
+
+    /**
+     * Once attached to the DOM, find all links embedded in the content and
+     * attach a listener to hijack their click event. This will allow us
+     * to hand things off to the router, acting the same as "v-link".
+     *
+     * @return {void}
+     */
+    attached() {
+        this.$nextTick(() => {
+            let links = this.$el.querySelectorAll('[data-link] a');
+            for (let i = 0, len = links.length; i < len; i++) {
+                links[i].addEventListener('click', e => {
+                    let path = links[i].getAttribute('href');
+
+                    if (path.charAt(0) === '/') {
+                        e.preventDefault();
+                        this.$route.router.go({ path });
+                    }
+                });
+            }
+        });
     },
 };
