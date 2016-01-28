bcef0c1d30a594650777fecc4fa6dbb195f42858
scottbedard <scottbedard90@gmail.com>
2016-01-27 22:58:35 -0700

    Add syntax highlighting for blog posts

--- assets/js/pages/blog/show.js
+++ assets/js/pages/blog/show.js
@@ -41,19 +41,23 @@ module.exports = {
     },
 
     /**
-     * Once attached to the DOM, find all links embedded in the content and
-     * attach a listener to hijack their click event. This will allow us
-     * to hand things off to the router, acting the same as "v-link".
-     *
      * @return {void}
      */
     attached() {
+
+        // Highlight any code blocks inside the post content
+        if (typeof Prism === 'object') {
+            Prism.highlightAll();
+        }
+
+        // Once attached to the DOM, find all links embedded in the content and
+        // attach a listener to hijack their click event. This will allow us
+        // to hand things off to the router, and work the same as v-link.
         this.$nextTick(() => {
             let links = this.$el.querySelectorAll('[data-link] a');
             for (let i = 0, len = links.length; i < len; i++) {
                 links[i].addEventListener('click', e => {
                     let path = links[i].getAttribute('href');
-
                     if (path.charAt(0) === '/') {
                         e.preventDefault();
                         this.$route.router.go({ path });
