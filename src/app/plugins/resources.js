2e2d7e6a79d3b78aa8d6602961d739daa4cead15
scottbedard <scottbedard90@gmail.com>
2016-10-01 17:23:37 -0700

    Attach X-CSRF-TOKEN to all requests

--- src/app/plugins/resources.js
+++ src/app/plugins/resources.js
@@ -3,6 +3,12 @@ import VueResource from 'vue-resource';
 
 Vue.use(VueResource);
 
+//
+// Append an X-CSRF-TOKEN to every post back to the server
+//
+let token = document.querySelector('meta[name=token]').getAttribute('content');
+Vue.http.headers.common['X-CSRF-TOKEN'] = token;
+
 Vue.use({
     install() {
         Vue.prototype.$resources = this.resources;
@@ -12,7 +18,7 @@ Vue.use({
         Object.keys(resources).forEach(key => {
             const promise = resources[key];
             promise.then(({ data, body }) => {
-        // @todo: figure out why data is a string on the dev server
+                // @todo: figure out why data is a string on the dev server
                 this[key] = typeof data === 'object' ? data : JSON.parse(body);
             });
             promises.push(promise);
