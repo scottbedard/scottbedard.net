719e5e9b9d32ddf44628bc1a3a7cb7faa07e7434
scottbedard <scottbedard90@gmail.com>
2016-10-02 17:20:51 -0700

    Test href buttons

--- test/unit/specs/ui/button.spec.js
+++ test/unit/specs/ui/button.spec.js
@@ -11,4 +11,14 @@ describe('button.vue', () => {
 
         expect(Object.values(vm.$el.classList).indexOf('color-red')).not.to.equal(-1);
     });
+
+    it('attaches an anchor when an href prop is passed in', () => {
+        const vm = new Vue({
+            el: document.createElement('div'),
+            components: { 'v-button': ButtonComponent },
+            render: h => <div><v-button href="http://scottbedard.net">></v-button></div>,
+        });
+
+        expect(vm.$el.querySelectorAll('a[href="http://scottbedard.net"]').length).to.equal(1);
+    });
 });
