9639ef5db8ae5150be320811590d35ba5bae2dd0
Steven Bedard <scottbedard90@gmail.com>
2018-09-29 21:42:48 -0700

    cleaning things up a bit, and adding a margin component

---
+++ themes/scottbedard/src/components/containers/margin.vue
@@ -0,0 +1,14 @@
+<script>
+import { bindAll } from 'spyfu-vue-functional';
+
+export default {
+    render(h, context) {
+        const bindings = bindAll(context);
+
+        return <div {...bindings}>
+            {context.slots().default}
+        </div>;
+    },
+    functional: true,
+};
+</script>
--- themes/scottbedard/src/components/global.js
+++ themes/scottbedard/src/components/global.js
@@ -1,8 +1,17 @@
 import buttonComponent from './ui/button.vue';
+import marginComponent from './containers/margin.vue';
 
 //
 // global components
 //
 export default {
+    //
+    // containers
+    //
+    'v-margin': marginComponent,
+
+    //
+    // ui
+    //
     'v-button': buttonComponent,
 };
