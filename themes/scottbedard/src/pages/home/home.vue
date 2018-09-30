27731f3c7d2a234d612c3e12e7cf00efa7a31fae
Steven Bedard <scottbedard90@gmail.com>
2018-09-30 10:10:05 -0700

    add transitions to home text

--- themes/scottbedard/src/pages/home/home.vue
+++ themes/scottbedard/src/pages/home/home.vue
@@ -2,10 +2,10 @@
     <div>
         <v-margin class="flex justify-center" padded>
             <div class="md:text-center">
-                <h1 class="font-thin mb-2 text-4xl md:mb-4 md:text-5xl">
+                <h1 class="font-thin mb-2 text-4xl transition-font md:mb-4 md:text-5xl">
                     Scott Bedard
                 </h1>
-                <h2 class="font-thin mb-2 text-2xl md:mb-4 md:text-3xl">
+                <h2 class="font-thin mb-2 text-2xl transition-font md:mb-4 md:text-3xl">
                     Welcome to my personal code playground
                 </h2>
             </div>
