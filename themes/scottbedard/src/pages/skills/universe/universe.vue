2a614b75c4e08ce14145c6c7dd6ec7d6e3df10eb
Scott Bedard <scottbedard90@gmail.com>
2018-10-04 20:24:12 -0700

    wire up animations for second and third orbits

--- themes/scottbedard/src/pages/skills/universe/universe.vue
+++ themes/scottbedard/src/pages/skills/universe/universe.vue
@@ -11,14 +11,14 @@
 
 .circle-2 {
     border-color: #dadada;
-    height: 340px;
-    width: 340px;
+    height: 350px;
+    width: 350px;
 }
 
 .circle-3 {
     border-color: #dfdfdf;
-    height: 500px;
-    width: 500px;
+    height: 538px;
+    width: 538px;
 }
 
 .skill {
@@ -26,33 +26,16 @@
         box-shadow: 0 0 24px rgba(0, 0, 0, 0.15);
     }
 }
-
-// @for $i from 1 through 3 {
-//     &:nth-child(#{ $i }) {
-//         animation: orbit-#{ $i } #{ $orbit-speed } linear 0s infinite;
-//         &:hover { animation-play-state: paused; .level { animation-play-state: paused }}
-//         .level {
-//             animation: level-#{ $i } #{ $orbit-speed } linear 0s infinite reverse;
-//             height: 90px;
-//             width: 90px;
-//         }
-//         img { width: 60px }
-//     }
-//     @keyframes level-#{ $i } {
-//         from { transform: rotate(#{ 0deg - ($i * 120) }) }
-//         to { transform: rotate(#{ 360deg - ($i * 120) }) }
-//     }
-//     @keyframes orbit-#{ $i } {
-//         from { transform: rotate(#{ 0deg + ($i * 120) }) translateX(70px) }
-//         to { transform: rotate(#{ 360deg + ($i * 120) }) translateX(70px) }
-//     }
-// }
 </style>
 
 <template>
     <div class="flex items-center justify-center relative universe">
         <!-- orbit animation keyframes -->
-        <v-dynamic-style :content="firstOrbitKeyframes" />
+        <v-dynamic-style :content="[
+            firstOrbitKeyframes,
+            secondOrbitKeyframes,
+            thirdOrbitKeyframes,
+        ]" />
 
         <!-- circles -->
         <div class="absolute border border-dotted circle-1 pin-center rounded-half z-0"></div>
@@ -63,20 +46,38 @@
         <div
             v-for="(skill, i) in firstOrbit"
             :key="skill.id"
-            :style="{
-                animation: `first-orbit-${i} 60s linear 0s infinite`,
-            }"
+            :style="{ animation: `orbit-1-${i} 90s linear 0s infinite` }"
             class="skill absolute flex h-20 items-center justify-center w-20">
-            <div 
-                class="bg-grey-lightest flex h-full items-center justify-center rounded-half w-full"
-                :style="{
-                    animation: `first-orbit-${i}-inner 60s linear 0s infinite`
-                }">
-                <img
-                    class="h-auto w-12"
-                    :alt="skill.name"
-                    :src="skill.logo.path"
-                />
+            <div
+                class="bg-white flex h-full items-center justify-center rounded-half w-full"
+                :style="{ animation: `orbit-1-${i}-inner 90s linear 0s infinite` }">
+                <img class="h-auto w-2/3" :alt="skill.name" :src="skill.logo.path" />
+            </div>
+        </div>
+
+        <!-- second orbit -->
+        <div
+            v-for="(skill, i) in secondOrbit"
+            :key="skill.id"
+            :style="{ animation: `orbit-2-${i} 180s linear 0s infinite` }"
+            class="skill absolute flex h-14 items-center justify-center w-14">
+            <div
+                class="bg-white flex h-full items-center justify-center rounded-half w-full"
+                :style="{ animation: `orbit-2-${i}-inner 180s linear 0s infinite` }">
+                <img class="h-auto w-3/5" :alt="skill.name" :src="skill.logo.path" />
+            </div>
+        </div>
+
+        <!-- third orbit -->
+        <div
+            v-for="(skill, i) in thirdOrbit"
+            :key="skill.id"
+            :style="{ animation: `orbit-3-${i} 240s linear 0s infinite` }"
+            class="skill absolute flex h-8 items-center justify-center w-8">
+            <div
+                class="bg-white flex h-full items-center justify-center rounded-half w-full"
+                :style="{ animation: `orbit-3-${i}-inner 240s linear 0s infinite` }">
+                <img class="h-auto w-3/5" :alt="skill.name" :src="skill.logo.path" />
             </div>
         </div>
     </div>
@@ -98,17 +99,59 @@ export default {
             return this.activeSkills.filter(skill => skill.orbit === 1);
         },
         firstOrbitKeyframes() {
-            const degreeOffset = 360 / this.firstOrbit.length;
+            const offset = 360 / this.firstOrbit.length;
 
             return this.firstOrbit.map((s, i) => {
-                const rotation = i * degreeOffset;
+                const rotation = i * offset;
 
                 return `
-                    @keyframes first-orbit-${i} {
-                        from { transform: rotate(${rotation}deg) translateX(64px) }
-                        to { transform: rotate(${rotation + 360}deg) translateX(64px) }
+                    @keyframes orbit-1-${i} {
+                        from { transform: rotate(${rotation}deg) translateX(68px) }
+                        to { transform: rotate(${rotation + 360}deg) translateX(68px) }
                     }
-                    @keyframes first-orbit-${i}-inner {
+                    @keyframes orbit-1-${i}-inner {
+                        from { transform: rotate(-${rotation}deg) }
+                        to { transform: rotate(-${rotation + 360}deg) }
+                    }
+                `;
+            });
+        },
+        secondOrbit() {
+            return this.activeSkills.filter(skill => skill.orbit === 2);
+        },
+        secondOrbitKeyframes() {
+            const offset = 360 / this.secondOrbit.length;
+
+            return this.secondOrbit.map((s, i) => {
+                const rotation = i * offset;
+
+                return `
+                    @keyframes orbit-2-${i} {
+                        from { transform: rotate(${rotation}deg) translateX(170px) }
+                        to { transform: rotate(${rotation + 360}deg) translateX(170px) }
+                    }
+                    @keyframes orbit-2-${i}-inner {
+                        from { transform: rotate(-${rotation}deg) }
+                        to { transform: rotate(-${rotation + 360}deg) }
+                    }
+                `;
+            });
+        },
+        thirdOrbit() {
+            return this.activeSkills.filter(skill => skill.orbit === 3);
+        },
+        thirdOrbitKeyframes() {
+            const offset = 360 / this.thirdOrbit.length;
+
+            return this.thirdOrbit.map((s, i) => {
+                const rotation = i * offset;
+
+                return `
+                    @keyframes orbit-3-${i} {
+                        from { transform: rotate(${rotation}deg) translateX(268px) }
+                        to { transform: rotate(${rotation + 360}deg) translateX(268px) }
+                    }
+                    @keyframes orbit-3-${i}-inner {
                         from { transform: rotate(-${rotation}deg) }
                         to { transform: rotate(-${rotation + 360}deg) }
                     }
