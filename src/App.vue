2d769499477855e53f5999ee75bf00864182b8df
Scott Bedard <scottbedard90@gmail.com>
2021-02-12 23:02:14 -0700

    fresh start

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
-
-<style lang="scss">
+<style lang="postcss">
 html {
   @apply flex h-full items-center justify-center w-full;
-}
-
-body {
-  @apply text-4xl;
 }
 </style>
 
 <template>
-  <div>
-    Soon...
-  </div>
+  <Logo size="24" />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Logo from './components/logo.vue';
+
+export default defineComponent({
+  components: {
+    Logo
+  }
+})
+</script>
---
+++ src/components/logo.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    class="feather feather-box"
+    fill="#fff"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    stroke="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    :height="size"
+    :width="size"
+    :viewBox="`0 0 24 24`">
+    <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"/>
+    <polyline points="3.27 6.96 12 12.01 20.73 6.96"/>
+    <line x1="12" y1="22.08" x2="12" y2="12"/>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    size: {
+      default: '24',
+      type: String
+    }
+  }
+})
+</script>
