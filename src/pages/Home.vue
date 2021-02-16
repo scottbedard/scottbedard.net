6d1eb1834b71876d75c66797a0837d2654aa70a5
Scott Bedard <scottbedard90@gmail.com>
2021-02-15 23:03:13 -0700

    copy

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,12 +3,12 @@
     <div class="grid gap-6 text-center lg:col-span-3 lg:text-left xl:col-span-1">
       <h1 class="font-semibold text-6xl">Hello,</h1>
       <h2 class="font-semibold text-4xl">My name is Scott Bedard.</h2>
-      <div class="gap-6 grid max-w-3xl mx-auto text-lg">
+      <div class="gap-6 grid max-w-4xl mx-auto text-lg">
         <p>
-          I'm a frontend developer at <a href="https://chess.com">Chess.com</a>, welcome to my personal code playground. I keep all kinds of weird experiments here. Most were made for fun, so be careful drawing meaningful conclusions from them.
+          I'm a frontend developer at <a href="https://chess.com">Chess.com</a>, welcome to my personal code playground. I keep all sorts of weird experiments here. Most things here were just for fun, so take everything with a grain of salt. 😉
         </p>
         <p>
-          <span class="lg:hidden">Below</span><span class="hidden lg:inline">To the right</span> is my <em>current</em> universe of tools. Closest to the center is what I reach for every day.
+          <span class="lg:hidden">Below</span><span class="hidden lg:inline">To the right</span> is my current universe of tools. Closest to the center are the ones I reach for every day.
         </p>
       </div>
     </div>
