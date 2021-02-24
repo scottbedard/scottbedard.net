39c14ce26cb4bfeba1dcb024e5e306fc7e6f671e
Scott Bedard <scottbedard90@gmail.com>
2021-02-23 20:01:43 -0700

    use layout

--- src/pages/experiments/CssCube.vue
+++ src/pages/experiments/CssCube.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>Hello from the CSS cube page</div>
+  <Layout>
+    Hello from the CSS cube page
+  </Layout>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Layout from '@/components/Layout.vue'
+
+export default defineComponent({
+  components: {
+    Layout
+  }
+})
+</script>
