008b1f8e16f8491996b1445342bb3be474351183
Scott Bedard <scottbedard90@gmail.com>
2021-02-27 20:39:27 -0700

    turn scaffolding

--- src/components/twister/Twister.vue
+++ src/components/twister/Twister.vue
@@ -1,7 +1,8 @@
 <template>
   <!-- idle -->
-  <div class="scene">
+  <div class="scene" @transitionend="startNextTurn">
     <div class="core">
+      <!-- idle -->
       <div
         v-for="face in faces"
         class="face"
@@ -17,6 +18,8 @@
           :data-value="sticker.value"
           :key="index" />
       </div>
+
+      <!-- turning -->
       <div
         v-for="face in faces"
         class="face"
@@ -35,11 +38,14 @@
       </div>
     </div>
   </div>
+
+  <pre class="mt-32">{{ { currentTurn, queue } }}</pre>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref, onMounted, watch } from 'vue'
+import { computed, defineComponent, nextTick, ref, onMounted, watch } from 'vue'
 import { Cube, CubeFace } from '@bedard/twister'
+import { debounce } from 'lodash-es';
 
 const faces: CubeFace[] = ['u', 'l', 'f', 'r', 'b', 'd']
 
@@ -50,9 +56,11 @@ export default defineComponent({
     }
   },
   setup(props, { emit }) {
+    const animation = ref(false)
+
     const cube = ref(new Cube({ size: 3 }))
 
-    const currentTurn = ref('')
+    const currentTurn = computed(() => queue.value[0] || null)
 
     const turningStickers = computed(() => {
       return currentTurn.value
@@ -62,19 +70,29 @@ export default defineComponent({
 
     const turning = computed(() => turningStickers.value.length > 0)
 
+    const queue = ref([] as string[]);
+
+    const scramble = () => {
+      queue.value = ['U']
+      // queue.value = cube.value.generateScramble(5).split(' ')
+    }
+
+    const startNextTurn = debounce(() => queue.value.shift(), 10)
+
     onMounted(() => {
       cube.value.reset()
     })
 
     watch(props, () => {
-      currentTurn.value = 'U'
-      // if (!turning.value) cube.value.scramble(15)
+      if (!turning.value) scramble()
     })
 
     return {
       cube,
       currentTurn,
       faces,
+      queue,
+      startNextTurn,
       turning,
       turningStickers,
     }
@@ -140,8 +158,8 @@ $spacing: 5%;
 
 .sticker {
   aspect-ratio: 1 / 1;
-  border: 1px solid rgba(0, 0, 0, 0.5);
-  border-radius: $radius;
+  // border: 1px solid rgba(0, 0, 0, 0.5);
+  // border-radius: $radius;
 
   &[data-value="0"] { @apply bg-yellow-300 }
   &[data-value="1"] { @apply bg-yellow-600 }
@@ -153,9 +171,19 @@ $spacing: 5%;
 
 .turn-u {
   &[data-face="u"] { transform: rotateX(90deg) rotateZ(90deg) rotateY(-90deg) translateX($elevation) rotateY(90deg) }
-  &[data-face="l"] { transform: rotateY(0deg) rotateY(-270deg) translateX($elevation) rotateY(90deg) }
+  &[data-face="l"] { transform: rotateY(-180deg) rotateY(-90deg) translateX($elevation) rotateY(90deg) }
   &[data-face="f"] { transform: rotateY(-180deg) translateX($elevation) rotateY(90deg) }
   &[data-face="r"] { transform: rotateY(0deg) rotateY(-90deg) translateX($elevation) rotateY(90deg) }
   &[data-face="b"] { transform: rotateY(-270deg) rotateY(-90deg) translateX($elevation) rotateY(90deg) }
+  &[data-face="d"] { transform: rotateX(-90deg) rotateY(-90deg) translateX($elevation) rotateY(90deg) }
 }
+
+// .turn-l {
+//   &[data-face="u"] { transform: rotateX(90deg) rotateZ(-90deg) rotateY(-90deg) translateX($elevation) rotateY(90deg) }
+//   &[data-face="l"] { transform: rotateY(-90deg) rotateY(-90deg) translateX($elevation) rotateY(90deg) }
+//   &[data-face="f"] { transform: rotateY(-90deg) translateX($elevation) rotateY(90deg) }
+//   &[data-face="r"] { transform: rotateY(90deg) rotateY(-90deg) translateX($elevation) rotateY(90deg) }
+//   &[data-face="b"] { transform: rotateY(-180deg) rotateY(-90deg) translateX($elevation) rotateY(90deg) }
+//   &[data-face="d"] { transform: rotateX(-90deg) rotateY(-90deg) translateX($elevation) rotateY(90deg) }
+// }
 </style>
